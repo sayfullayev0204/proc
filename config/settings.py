cbe8ac13ee4436213771e3ba45b59f5c679591a7
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-10-12 05:56:14 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','*']
+ALLOWED_HOSTS = [.vercel.app','*']
 
 
 # Application definition
